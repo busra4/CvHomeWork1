434e0028eb9c6c7f06899e52f02fdf8bdf581c51
busra4 <76951800+busra4@users.noreply.github.com>
2022-03-22 15:03:43 +0300

    Update ScrollUp.js

--- my-app/src/components/ScrollUp.js
+++ my-app/src/components/ScrollUp.js
@@ -6,7 +6,7 @@ const ScrollUp = () => {
 
 	useEffect(() => {
 		window.addEventListener('scroll', () => {
-			if (window.scrollY > 400) {
+			if (window.scrollY > 500) {
 				setShowTopBtn(true);
 			} else {
 				setShowTopBtn(false);
